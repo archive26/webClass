845285b7e27b850c43afd5bced083b42f49313f1
archive26 <ahnna_98@naver.com>
2023-05-22 16:20:04 +0900

    1

--- 36. js-문서객체/js/study5.js
+++ 36. js-문서객체/js/study5.js
@@ -1,7 +1,21 @@
 window.onload = function(){
    let btn1 = document.querySelector(".btnClick")
    btn1.addEventListener("click",function(){
-    //클릭했을 때 실행되는 소스코드
+    //클릭했을 때 실행되는 소스코드 (이벤트형태,클릭했을떄 일어나는동작)
+    //클릭이벤트핸들러를 추가
     document.querySelector(".txtbox").style.color="black"
    })
+
+   let btnOpen = document.querySelector(".btnPopup")
+   btnOpen.addEventListener("click",function(){
+      document.querySelector(".hiddenPopup").classList.add("show")
+   })
+
+   let btnClose = document.querySelector(".btnPopupClose")
+   btnClose.addEventListener("click",function(){
+      document.querySelector(".hiddenPopup").classList.remove("show")
+   })
+
 }
+
+
