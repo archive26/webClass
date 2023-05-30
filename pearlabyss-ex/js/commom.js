ecc4ccf27fa5698b778e1595547d3b4d69b9279c
archive26 <ahnna_98@naver.com>
2023-05-30 17:37:37 +0900

    2

--- pearlabyss-ex/js/commom.js
+++ pearlabyss-ex/js/commom.js
@@ -13,5 +13,19 @@ window.onload = function(){
                 slidesPerView : 3,
             }
           }
-    });   
-}
+    });  
+    let newsSlider2 = new Swiper(".news_station",{
+        
+        pagination: {
+            el: '.swiper-pagination',
+          },
+          breakpoints:{
+            280:{
+                slidesPerView : 1,
+            },
+            768:{
+                slidesPerView : 3,
+            }
+          }
+    });      
+} 
