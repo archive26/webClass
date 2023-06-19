7a1de061ab08268f9604a389b6ae9e2ae71b85d3
archive26 <ahnna_98@naver.com>
2023-06-19 14:52:22 +0900

    web

---
+++ 40.effect/js/study3.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+
+    $(".btn").click(function(){
+        // textAni(".title",{
+        //     aniDelay:0.5,
+        //     aniSpeed:1,
+        //     textSize:70
+        // })
+        textAni(".title",{
+            textSize:100
+        })
+    })
+
+})
