ef65313421c38fbe405f3edec6bd87bf9525b6c5
archive26 <ahnna_98@naver.com>
2023-06-28 13:26:31 +0900

    ww

--- 41.svg/js/study3.js
+++ 41.svg/js/study3.js
@@ -1,31 +1,66 @@
-$(document).ready(function(){
-    
+$(document).ready(function () {
 
-    $(".btn").click(function(){
-
-        if($(".skill").hasClass("on")==false){
-            //skill클래스에 on클래스가 없다면
-            $(".skill").addClasss("on")
-            $(".skill>li").each(function(){
-                let list = $(this)
-                let percent = $(this).find(".per").text()
-                let count = 0
-                let circle = $(this).find("circle")
-    
-                let timer = setInterval(function(){
-                    count++
-                    list.find(".per").text(count+"%")
-                    circle.css("stroke-dashoffset",314-(314*(count/100)))
-                    if(count>=percent){
-                        clearInterval(timer)
-                    }
-                },30)     
-    
-    
-            })
-
-        }
-      
+    $(".btn").click(function () {
+  
+      if ($(".skill").hasClass("on") == false) {
+        //skill클래스에 on클래스가 없다면
+        //아래 코드를 실행함
+        $(".skill").addClass("on")
+  
+        $(".skill>li").each(function () {
+          let list = $(this)
+          let percent = $(this).find(".per").text()
+          let count = 0
+          let circle = $(this).find("circle")
+  
+          let timer = setInterval(function () {
+  
+            count++
+            list.find(".per").text(count + "%")
+            circle.css("stroke-dashoffset", 314 - (314 * (count / 100)))
+            if (count >= percent) {
+              clearInterval(timer)
+            }
+          }, 10)
+        })
+      }
     })
 
-})
+    $(window).scroll(function(){
+        //스크롤바가 움직일 때 마다 동작 실행
+        let a = $(window).scrollTop()
+        let b = $(".skill").offset().top
+        let c = $(window).height()*0.5
+
+        if(a+c>=b){
+            if ($(".skill").hasClass("on") == false) {
+                //skill클래스에 on클래스가 없다면
+                //아래 코드를 실행함
+                $(".skill").addClass("on")
+          
+                $(".skill>li").each(function () {
+                  let list = $(this)
+                  let percent = $(this).find(".per").text()
+                  let count = 0
+                  let circle = $(this).find("circle")
+          
+                  let timer = setInterval(function () {
+          
+                    count++
+                    list.find(".per").text(count + "%")
+                    circle.css("stroke-dashoffset", 314 - (314 * (count / 100)))
+                    if (count >= percent) {
+                      clearInterval(timer)
+                    }
+                  }, 10)
+                })
+              }
+        }
+    })
+
+
+  })
+
+
+  
+
---
+++ 41.svg/js/study4.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+    // let pathLength =  $(".letter").get(0).getTotalLength()
+    // // alert(pathLength)
+    // $(".letter").css("stroke-dasharray",pathLength)
+    // $(".letter").css("stroke-dashoffset",pathLength)
+
+    $("#introtxt path").each(function(){
+        
+    })
+
+})
